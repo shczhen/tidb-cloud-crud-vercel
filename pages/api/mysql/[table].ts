6b9efdfcc744dede5709b36ec1d03a942fd1d430
czhen <56986964+shczhen@users.noreply.github.com>
2022-07-19 14:01:29 +0800

    test dynamic route

---
+++ pages/api/mysql/[table].ts
@@ -0,0 +1,20 @@
+import { connection } from "./_index";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// Main handler function
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const { table: tableName } = req.query;
+
+  try {
+    const results = await connection.query(`describe ?`, [tableName]);
+    res.status(200).json({ data: results });
+  } catch (error) {
+    res.status(500).send(error);
+  } finally {
+    // Run clean up function
+    await connection.end();
+  }
+}
---
+++ pages/api/mysql/_index.ts
@@ -0,0 +1,28 @@
+import serverlessMysql from "serverless-mysql";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export const connection = serverlessMysql({
+  config: {
+    host: process.env.TIDB_URI,
+    database: (process.env.TIDB_DB as string) || "test",
+    user: (process.env.TIDB_USER as string) || "root",
+    password: process.env.TIDB_PASSWD,
+    port: parseInt(process.env.TIDB_DB || "4000"),
+  },
+});
+
+// // Main handler function
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<any>
+// ) {
+//   // Run your query
+//   let results = await connection.query("describe expresses;");
+
+//   // Run clean up function
+//   await connection.end();
+
+//   // Return the results
+//   // return results;
+//   res.status(200).json({ data: results });
+// }
